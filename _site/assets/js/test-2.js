edc6f0bd64892335d98e973a7a0984dd361eeb03
Jianhua Shi <shijh09@sina.com>
2016-01-14 17:35:29 +0800

    11 post

---
+++ _site/assets/js/test-2.js
@@ -0,0 +1,16 @@
+﻿function refreshPrice(data) {
+    var p = document.getElementById('test-jsonp');
+    p.innerHTML = '当前城市：' +
+        data['city'].name +': ' + 
+        data['0000001'].price + '；' +
+        data['1399001'].name + ': ' +
+        data['1399001'].price;
+}
+function getPrice() {
+    var
+        js = document.createElement('script'),
+        head = document.getElementsByTagName('head')[0];
+	
+    js.src = 'http://wthrcdn.etouch.cn/weather_mini?citykey=101010100';
+    head.appendChild(js);
+}
